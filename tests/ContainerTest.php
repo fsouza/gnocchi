a377506a96e724db33adb34f389b7b6edf00b826
Francisco Souza <francisco@franciscosouza.net>
2010-08-03 14:04:05 -0300

    Added basic directory structure

---
+++ tests/ContainerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+
+class ContainerTest extends PHPUnit_Framework_TestCase {
+
+    public function testStupidAssert() {
+        $this->assertEquals(1, 1);
+    }
+
+}

c4e8e8132b6a93de109ee56a7695b192f9636cce
Francisco Souza <francisco@franciscosouza.net>
2010-08-03 18:49:49 -0300

    Some docs on GnocchiContainer

--- lib/GnocchiContainer.php
+++ lib/GnocchiContainer.php
@@ -8,9 +8,27 @@
 class GnocchiContainer {
 
     var $classLoader;
+    private $components;
 
+    /**
+     * Container constructor, has no parameters, just construct the object inside.
+     */
     public function __construct() {
         $this->classLoader = new GnocchiClassLoader();
+        $this->components = array();
+    }
+
+    /**
+     * Add a component to be managed by Gnocchi.
+     *
+     * @param $component: The component name or the component object (instance of GnocchiComponent)
+     */
+    public function addComponent($component) {
+        if ($component instanceof GnocchiComponent) {
+            $this->components[$component->getClassName()] = $component;
+        } else {
+            $this->components[$component] = new GnocchiComponent($component);
+        }
     }
 
 }

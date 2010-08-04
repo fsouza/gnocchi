1d6c14479fa7172b6c9a711828ced0c0e1159189
Francisco Souza <francisco@franciscosouza.net>
2010-08-04 13:16:06 -0300

    Added method to return parameters with its values in GnocchiComponent

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -23,11 +23,38 @@ class ComponentTest extends PHPUnit_Framework_TestCase {
         $component = new GnocchiComponent('UnknowClass');
     }
 
+    /**
+     * @expectedException ReflectionException
+     */
+    public function testRaisesExcpetionGettingValuesForParametersWithouValues() {
+        $component = new GnocchiComponent('Person');
+        $parameters = $component->getConstructorParametersWithValues();
+    }
+
+    /**
+     * @dataProvider classAndParametersProvider
+     */
+    public function testSetValueForConstructorParameter($class, $params) {
+        $component = new GnocchiComponent($class);
+        foreach ($params as $param => $value) {
+            $component->setConstructorParameterValue($param, $value);
+        }
+        $this->assertEquals($component->getConstructorParametersWithValues(), $params);
+    }
+
     public function expectCountProvider() {
         return array(
                 array('Person', 1),
                 array('Car', 2),
                 array('Chicken', 0)
+            );
+    }
+
+    public function classAndParametersProvider() {
+        return array(
+                array('Person', array('name' => 'Barack Obama')),
+                array('Car', array('name' => 'C300', 'brand' => 'Chrysler')),
+                array('Chicken', array())
             );
     }
 

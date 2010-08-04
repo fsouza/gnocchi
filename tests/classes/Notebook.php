af88febc3c022820991a97224c7f2c0dc318f5ab
Francisco Souza <francisco@franciscosouza.net>
2010-08-04 13:23:06 -0300

    Added missing test sample class (Notebook)

---
+++ tests/classes/Notebook.php
@@ -0,0 +1,12 @@
+<?php
+
+class Notebook {
+
+    private $name, $color;
+
+    public function __construct($name, $color = 'black') {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+}
